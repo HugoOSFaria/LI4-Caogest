c1677e6c14983dcb9ff9f1964b8f41643b5f4653
João Diogo Mota <36596261+JoaoDiogoMota@users.noreply.github.com>
2020-03-24 18:50:13 +0000

    Add files via upload

---
+++ DB/src/DbConnect.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Add MySQL library
+using MySql.Data.MySqlClient;
+
+namespace CaoGest.src
+{
+    class DbConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string dbId;
+        private string dbPassword;
+
+        //assign the atributes
+        public DbConnect()
+        {
+            server = "localhost";
+            database = "caogest";
+            dbId = "root";
+            dbPassword = "root";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + dbId + ";" + "PASSWORD=" + dbPassword + ";";
+            connection = new MySqlConnection(connectionString);
+        }
+
+        //open connection to database
+        private bool OpenConnection(){
+            try
+            {
+                connection.Open();
+                return true; 
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break; 
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        //Insert statement
+        public void Insert()
+        {
+            string query = "INSERT INTO tableinfo (name, age) VALUES('John Smith', '33')";
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+        //Update statement
+        public void Update(string nome)
+        {
+            string query = "UPDATE Canil SET CapacidadeTotal=30 WHERE Nome=nome";
+
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //create mysql command
+                MySqlCommand cmd = new MySqlCommand();
+                //Assign the query using CommandText
+                cmd.CommandText = query;
+                //Assign the connection using Connection
+                cmd.Connection = connection;
+
+                //Execute query
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+        //Delete statement
+        public void Delete()
+        {
+            string query = "DELETE FROM tableinfo WHERE name='John Smith'";
+
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+
+
+    }
+}
---
+++ DB/src/MainClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaoGest.src
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            DbConnect dC= new DbConnect();
+            String s = "CaoSeguro";
+            dC.Update(s);
+        }
+    }
+}
