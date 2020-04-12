8372eef8f69e431fb7963c93fe579353cb6d46ef
PSilva3 <36642980+PSilva3@users.noreply.github.com>
2020-04-12 15:23:53 +0100

    Update Queries

--- Func.cs
+++ Func.cs
@@ -2,98 +2,184 @@ using System;
 
 namespace ConsoleApp1{
     class Program{
-        void iniciarSessao(string email, string password){
-            if (utilizador.containsKey(email) && utilizador.valPalavra(password)){
+        //-----------------------------------------------------------------------------------------------Iniciar Sessão---------------------------------------------------------------------
+        void iniciarSessaoU(string email, string password)
+        {
+            if (canilDB.containsKey(email) && canilDB.valPalavra(password))
+            {
                 //Informar para reabrir a pagina
             }
-            else{
+            else
+            {
                 //Informar o site que não foi reconhecido o email ou a palavra-passe
             }
         }
-        void iniciarSessao(string email, string password){
-            if (utilizador.containsKey(email) && utilizador.valPalavra(password)){
+
+        void iniciarSessaoCanil(string email, string password)
+        {
+            if (canilDB.containsKey(email) && canilDB.valPalavra(password))
+            {
                 //Informar para reabrir a pagina
             }
-            else{
+            else
+            {
                 //Informar o site que não foi reconhecido o email ou a palavra-passe
             }
         }
-        void terminarSessao(){
+
+        void terminarSessao()
+        {
             //... acho que não é preciso fazer nada deste lado...
         }
-        void adicionarUtilizador(string email, string password, string nome, int cc, DateTime dataNasc, int idade, string morada){
-            if (utilizadores.containsKey(email)){
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+        //----------------------------------------------------------------------------------------------Funções Utilizador-----------------------------------------------------------------------------
+        void adicionarUtilizador(string email, string password, string nome, int cc, DateTime dataNasc, int idade, string morada)
+        {
+            if (canilDB.containsKey(email))
+            {
                 //Informar que mail ja existe
             }
-            else{
-                utilizadores.adicionarBD(email, password, nome, cc, dataNasc, idade, morada);
+            else
+            {
+                canilDB.adicionarBD(email, password, nome, cc, dataNasc, idade, morada);
             }
         }
-        void alterarPalU(string email, string nPass){
-            utilizadores.alterarPal(email, nPass);
+
+        void removerUtilizador(string email)
+        {
+            canilDB.removerUtilizador(email);
         }
-        void alterarEmailU(string email, string nPass){
-            utilizadores.editarEmail(email,novoEmail);
+
+        void alterarPalU(string email, string nPass)
+        {
+            canilDB.alterarPal(email, nPass);
         }
-        void alterarPalC(string email, string nPass){
-            canil.alterarPal(email, nPass);
+
+        void alterarEmailU(string email, string nPass)
+        {
+            canilDB.editarEmail(email, novoEmail);
         }
-        void alterarEmailC(string email, string nPass){
-            canil.editarEmail(email,novoEmail);
+
+        void registaHorario(string emailC, string emailV, DateTime horarioI, DateTime horarioF)
+        {
+            canilDB.adicionarCanil(emailC, emailV, horarioI, horarioF);
         }
-        void adicionarCao(string emailC,char genero, string nome, string cor, string raca, int idade, string descricao){
-            int caoId = caes.adicionarCao(genero, nome, cor, raca, idade, descricao);
-            canil.adicionarCao(emailC, caoID);
+
+        void desmarcaHorario(string emailC, string emailV, DateTime horarioI, DateTime horarioF)
+        {
+            canilDB.desmarcaHorario(emailC, emailV, horarioI, horarioF);
         }
-        void adicionarCanil(string nome, string email, string password, int cap, string morada, int tel, int nib){
-            canis.adicionarCanil(nome, email, password, cap, morada, tel, nib);
+
+        void favoritarCao(string email, int caoID)
+        {
+            canilDB.favoritarCao(email, caoID);
         }
-        void adicionarParceria(string emailC, string nome, string info){
-            canis.adicionarParceria(emailC, nome, info);
+
+        void desfavoritarCao(string email, int caoID)
+        {
+            canilDB.favoritarCao(email, caoID);
         }
-        void adicionaHorario(string emailC, DateTime horarioI, DateTime horarioF){
-            canis.adicionarHorario(emailC, horarioI, horarioF);
+
+        void adotarCao(string email, int caoID)
+        {
+            canilDB.adotarCao(email, caoID);
+            canilDB.adotarCao(caoID);
         }
-        void registaHorario(string emailC, string emailV, DateTime horarioI, DateTime horarioF){
-            canis.adicionarCanil(emailC, emailV, horarioI, horarioF);
+
+        void pesquisaCao(string raca, int idade, string local)
+        {
+            canilDB.pesquisaCao(local);
         }
-        void favoritarCao(string email, int caoID){
-            utilizador.favoritarCao(email, caoID);
+
+        void pesquisaCao(string raca, int idade, char sexo, string cor, char porte, string local)
+        {
+            canilDB.pesquisaCao(raca, idade, sexo, cor, porte, local);
         }
-        void adotarCao(string email, int caoID){
-            utilizador.adotarCao(email, caoID);
-            caes.adotarCao(caoID);
+
+        void pesquisaCaoCanil(string raca, int idade, char sexo, string cor, char porte, string emailC)
+        {
+            canilDB.pesquisaCaoCanil(raca, idade, sexo, cor, porte, emailC);
         }
-        void removerUtilizador(string email){
-            utilizadores.removerUtilizador(email);
+
+        void pesquisaCanil(string local)
+        {
+            canilDB.canisProximos(local);
         }
-        void removerCao(string emailC,int caoID){
-            canil.removerCao(emailC, caoID);
-            caes.removerCao(caoID);
+
+        void verParcerias(string emailC)
+        {
+            List<List<String>> list = new List<List<String>>();
+            list = canilDB.SelectParcerias(emailC);
+            //Passar lista para o encapsulamento e mandar para site
         }
-        void removerCanil(string email){
-            canis.removerCanil(email);
+
+        //----------------------------------------------------------------------------------------------Funções Canis-----------------------------------------------------------------------------
+        void adicionarCanil(string email, string nome, string password, int capO, int capT, string distrito, string rua, string local, string contacto, string nib)
+        {
+            if (canilDB.containsKey())
+            {
+                //mensagem de erro
+            }
+            else canilDB.insereCanil(email, nib, nome, capO, capT, password, distrito, rua, local, contacto);
         }
-        void removerParceria(string emailC, string nome){
-            canis.removerParceria(emailC, nome);
+
+        void removerCanil(string email)
+        {
+            canilDB.deleteCanil(email);
         }
-        void desmarcaHorario(string emailC, string emailV, DateTime horarioI, DateTime horarioF){
-            canis.desmarcaHorario(emailC, nome, horarioI, horarioF);
+
+        void alterarPalC(string email, string nPass)
+        {
+            canilDB.updatePass(email, nPass);
         }
-        void removeHorario(string emailC, DateTime horarioI, DateTime horarioF){
-            canis.removeHorario(emailC, horarioI, horarioF);
+
+        void alterarEmailC(string email, string nEmail)
+        {
+            canilDB.updateContacto(email, nEmail);
         }
-        void pesquisaCanil(string local){
-            canils.canisProximos(local);
+
+        void adicionarParceria(string emailC, string nome, string info)
+        {
+            canilDB.adicionarParceria(emailC, nome, info);
         }
-        void pesquisaCao(string raca, int idade, string local){
-            canils.pesquisaCao(local);
+
+        void removerParceria(string emailC, string nome)
+        {
+            canilDB.removerParceria(emailC, nome);
         }
-        void pesquisaCao(string raca, int idade, char sexo, string cor, char porte, string local){
-            canils.pesquisaCao(raca, idade, sexo, cor, porte, local);
+
+        void adicionaHorario(string emailC, DateTime horarioI, DateTime horarioF)
+        {
+            canilDB.adicionarHorario(emailC, horarioI, horarioF);
         }
-        void pesquisaCaoCanil(string raca, int idade, char sexo, string cor, char porte, string emailC){
-            canils.pesquisaCaoCanil(raca, idade, sexo, cor, porte, emailC);
+
+        void removeHorario(string emailC, DateTime horarioI, DateTime horarioF)
+        {
+            canilDB.removeHorario(emailC, horarioI, horarioF);
+        }
+
+        void adicionarCao(string emailC, char genero, string nome, string cor, string raca, int idade, string descricao)
+        {
+            int caoId = canilDB.adicionarCao(genero, nome, cor, raca, idade, descricao);
+            canilDB.adicionarCao(emailC, caoID);
+        }
+
+        void removerCao(string emailC, int caoID)
+        {
+            canilDB.removerCao(emailC, caoID);
+            canilDB.removerCao(caoID);
+        }
+
+        void updateCapT(string email, int capT)
+        {
+            canilDB.updateCapacidadeT(email, capT);
+        }
+
+        void updateCapO(string email, int capO)
+        {
+            canilDB.updateCapacidadeO(email, capO);
         }
     }
 }
