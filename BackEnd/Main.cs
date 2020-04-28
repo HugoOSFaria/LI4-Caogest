f76a7040110379726fafc3c72e89a6faa3384a5a
asergiogomes <ae3654@alunos.uminho.pt>
2020-04-28 21:17:42 +0100

    main de teste da encriptacao

--- BackEnd/Main.cs
+++ BackEnd/Main.cs
@@ -7,8 +7,32 @@ using System.Net.Sockets;
 
 namespace LI4BackEnd
 {
-    class Main
+    class Teste
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a password to use:");
+            string password = Console.ReadLine();
+        Console.WriteLine("Please enter a string to encrypt:");
+            string plaintext = Console.ReadLine();
+        Console.WriteLine("");
+
+            Console.WriteLine("Your encrypted string is:");
+            string encryptedstring = Encriptar.Encriptacao(plaintext, password);
+        Console.WriteLine(encryptedstring);
+            Console.WriteLine("");
+
+            Console.WriteLine("Your decrypted string is:");
+            string decryptedstring = Encriptar.Desencriptacao(encryptedstring, password);
+        Console.WriteLine(decryptedstring);
+            Console.WriteLine("");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+}
+
+        /*
         static void main(string[] args)
         {
 
@@ -64,6 +88,6 @@ namespace LI4BackEnd
                 }
             }
         }
+        */
 
-    }
-}
+ }
