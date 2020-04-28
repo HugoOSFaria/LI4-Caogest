16de14113033d0a174d69c44e0667d3bbfc74555
asergiogomes <ae3654@alunos.uminho.pt>
2020-04-28 21:58:23 +0100

    commit

---
+++ BackEnd/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace LI4BackEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a password to use:");
+            string password = Console.ReadLine();
+            Console.WriteLine("Please enter a string to encrypt:");
+            string plaintext = Console.ReadLine();
+            Console.WriteLine("");
+
+            Console.WriteLine("Your encrypted string is:");
+            string encryptedstring = Encriptar.Encriptacao(plaintext, password);
+            Console.WriteLine(encryptedstring);
+            Console.WriteLine("");
+
+            Console.WriteLine("Your decrypted string is:");
+            string decryptedstring = Encriptar.Desencriptacao(encryptedstring, password);
+            Console.WriteLine(decryptedstring);
+            Console.WriteLine("");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
+
+       
