56094f30353764c9804967087692764d596dbe8a
PSilva3 <36642980+PSilva3@users.noreply.github.com>
2020-05-25 22:18:38 +0100

    Add files via upload

--- trial2/trial2/Controllers/StatsController.cs
+++ trial2/trial2/Controllers/StatsController.cs
@@ -70,7 +70,7 @@ namespace trial2.Controllers
 
         // GET: api/Status/Adocoes
         [HttpGet("Adocoes")]
-        public async Task<ActionResult<int>> GetStatsÃ()
+        public async Task<ActionResult<int>> GetStatsA()
         {
             var users = await (from a in _context.Adocao
                                where a.estado == "Aceite"
@@ -83,6 +83,75 @@ namespace trial2.Controllers
             int count = users.Count();
 
             return count;
+        }
+
+        // GET: api/Status/Pedidos/5
+        [HttpGet("Pedidos/Mes")]
+        public async Task<ActionResult<List<int>>> GetStatsPM()
+        {
+            var users = await (from a in _context.Adocao
+                               where a.estado == "Aceite"
+                               select a).ToListAsync();
+            
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            List<int> valores = new List<int>();
+            
+            for(int i=0; i< 12; i++) valores.Add(0);
+
+            foreach(Adocao a in users)
+            {
+                if (a.data.Month == 1) valores[0]++;
+                else if (a.data.Month == 2) valores[1]++;
+                else if (a.data.Month == 3) valores[2]++;
+                else if (a.data.Month == 4) valores[3]++;
+                else if (a.data.Month == 5) valores[4]++;
+                else if (a.data.Month == 6) valores[5]++;
+                else if (a.data.Month == 7) valores[6]++;
+                else if (a.data.Month == 8) valores[7]++;
+                else if (a.data.Month == 9) valores[8]++;
+                else if (a.data.Month == 10) valores[9]++;
+                else if (a.data.Month == 11) valores[10]++;
+                else valores[11]++;
+            }
+
+            return valores;
+        }
+
+        // GET: api/Status/Pedidos/Cao
+        [HttpGet("Pedidos/Cao")]
+        public async Task<ActionResult<List<int>>> GetStatsPC()
+        {
+            var pequenos = await (from a in _context.Adocao
+                                  join c in _context.Cao on a.cao_idCao equals c.idCao
+                                  where a.estado == "Aceite" && c.porte == "Pequeno"
+                                  select a).ToListAsync();
+
+            var medios = await (from a in _context.Adocao
+                                  join c in _context.Cao on a.cao_idCao equals c.idCao
+                                  where a.estado == "Aceite" && c.porte == "Medio"
+                                  select a).ToListAsync();
+            
+            var grandes = await (from a in _context.Adocao
+                                  join c in _context.Cao on a.cao_idCao equals c.idCao
+                                  where a.estado == "Aceite" && c.porte == "Grande"
+                                  select a).ToListAsync();
+
+            if (pequenos == null || medios == null || grandes == null)
+            {
+                return NotFound();
+            }
+            
+            List<int> valores = new List<int>();
+
+            valores.Add(pequenos.Count());
+            valores.Add(medios.Count());
+            valores.Add(grandes.Count());
+
+            return valores;
         }
     }
 }
